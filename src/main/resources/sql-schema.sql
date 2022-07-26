8603b612648b54e68e4c047e25f74e51c83dc9ad
subhaan malik <subhaanmalik@pop-os.localdomain>
2022-07-26 14:17:15 +0100

    connected SQL to Java

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,12 +1,39 @@
-drop schema ims;
+drop database if exists IMS; 
+CREATE DATABASE IF NOT EXISTS IMS;
+USE IMS;
 
-CREATE SCHEMA IF NOT EXISTS `ims`;
+CREATE TABLE customers (
+    id int AUTO_INCREMENT,
+    first_name varchar(40),
+    surname varchar(40),
+    PRIMARY KEY (id)
+    );
 
-USE `ims` ;
+CREATE TABLE items (
+    id INT AUTO_INCREMENT,
+    item_name varchar(40),
+    price decimal(9,3),
+    PRIMARY KEY (id)
+    );
 
-CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+
+CREATE TABLE orders (
+    id int AUTO_INCREMENT,
+    customer_id INT,
+    
+    PRIMARY KEY (id),
+    FOREIGN KEY (customer_id) 
+        REFERENCES customers(id)
+    );
+
+CREATE TABLE order_items (
+    id INT AUTO_INCREMENT,
+    item_id INT,
+    order_id INT,
+    quantity INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (order_id)
+        REFERENCES orders(id),
+    FOREIGN KEY (item_id)
+        REFERENCES items(id)
 );
