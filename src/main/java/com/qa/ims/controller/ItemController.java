8550c612f0156896962a3f698a5fa85685f35443
subhaan malik <subhaanmalik@pop-os.localdomain>
2022-07-27 16:11:53 +0100

    added itemDOA

--- src/main/java/com/qa/ims/controller/ItemController.java
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -2,15 +2,35 @@ package com.qa.ims.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
 
 public class ItemController implements CrudController<Item> {
 
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private ItemDAO itemDAO;
+	private Utils utils;
+
+	public ItemController(ItemDAO itemDAO, Utils utils) {
+		super();
+		this.itemDAO = itemDAO;
+		this.utils = utils;
+	}
+	
+	
 	@Override
 	public List<Item> readAll() {
-		System.out.println("Hello");
-		// TODO Auto-generated method stub
-		return null;
+		List<Item> items = itemDAO.readAll();
+		for (Item item : items) {
+			LOGGER.info(items);
+		}
+		return items;
 	}
 
 	@Override
--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -1,5 +1,77 @@
 package com.qa.ims.persistence.domain;
 
+import java.util.Objects;
+
 public class Item {
 
+	private Long id;
+	private String item_name;
+//	private String item_category;
+	private double price;
+	
+	public Item(Long id, String item_name, double price) {
+		this.setItem_name(item_name);
+//		this.setItem_category(item_category);
+		this.setPrice(price);
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getItem_name() {
+		return item_name;
+	}
+	public void setItem_name(String item_name) {
+		this.item_name = item_name;
+	}
+	
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+//	public String getItem_category() {
+//		return item_category;
+//	}
+//
+//	public void setItem_category(String item_category) {
+//		this.item_category = item_category;
+//	}
+	
+	
+	@Override
+	public String toString() {
+		return "id:" + id + " item name:" + item_name + "price: " + price;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, item_name, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		return id == other.id 
+				&& Objects.equals(item_name, other.item_name)
+				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price);
+	}
+
+
+	
 }
